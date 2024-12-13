59e3a5769ad842cb7812f877acffe7a1428249d9
JingmingLiu <jingmingliu01@outlook.com>
2024-12-12 20:10:54 -0500

    support pagination

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm
+from django.core.paginator import Paginator
 
 def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, 'task_list.html', {'tasks': tasks})
+    tasks = Task.objects.all()  # Query the Task model to get all tasks
+    paginator = Paginator(tasks, 5)  # every page shows 10 tasks
+    page_number = request.GET.get('page')  # get the current page number
+    page_obj = paginator.get_page(page_number)  # get the current page object
+    return render(request, 'task_list.html', {'tasks': page_obj})  # pass the page object to the template
 
 def add_task(request):
     if request.method == 'POST':
