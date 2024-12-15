645203e0512e05e35c3ea89c28ac5eb9c18738ed
JingmingLiu <jingmingliu01@outlook.com>
2024-12-15 17:02:50 -0500

    support search-by-title and select Today tasks

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     description = models.TextField(blank=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    duration = models.FloatField(editable=False)  # 自动计算，不允许用户手动输入
+    duration = models.FloatField(editable=False)
 
     def save(self, *args, **kwargs):
         self.duration = (self.end_time - self.start_time).total_seconds() / 3600.0  # 持续时间以小时为单位
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Task
 from .forms import TaskForm
 from django.core.paginator import Paginator
 
+
 def task_list(request):
+    query = request.GET.get('q', '') # get the search query from the request
+    today = request.GET.get('today') # get the today query from the request
     tasks = Task.objects.all().order_by('start_time') # Query the Task model to get all tasks, ordered by start_time
+    if today:
+        now = timezone.now().date()  # 获取今天的日期
+        tasks = tasks.filter(start_time__date__lte=now, end_time__date__gte=now)
+    if query:
+        tasks = tasks.filter(title__icontains=query) # filter the tasks by the search query
     paginator = Paginator(tasks, 5)  # every page shows 5 tasks
     page_number = request.GET.get('page')  # get the current page number
     page_obj = paginator.get_page(page_number)  # get the current page object
-    return render(request, 'task_list.html', {'tasks': page_obj})  # pass the page object to the template
+    return render(request, 'task_list.html',
+                  {'tasks': page_obj, 'query': query})  # pass the page object to the template
+
 
 def add_task(request):
     if request.method == 'POST':
@@ -20,6 +31,7 @@ def add_task(request):
         form = TaskForm()
     return render(request, 'add_task.html', {'form': form})
 
+
 def edit_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     if request.method == 'POST':
@@ -30,6 +42,7 @@ def edit_task(request, task_id):
     else:
         form = TaskForm(instance=task)
     return render(request, 'edit_task.html', {'form': form, 'task': task})
+
 
 def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
